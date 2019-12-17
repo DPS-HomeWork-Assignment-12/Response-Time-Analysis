33548d31962fdcc161a52601c86dc7bad5167799
mandali-harish <51310990+mandali-harish@users.noreply.github.com>
2019-12-17 10:20:42 +0100

    Add files via upload

--- Curve.java
+++ Curve.java
@@ -1,20 +1,25 @@
 import java.util.ArrayList;
 
 public class Curve {
-	ArrayList<ArrayList<Double>> curve = new ArrayList<ArrayList<Double>>();
-	//private ArrayList<Double> segment = new ArrayList<Double>();
-	public Curve(ArrayList<Double> co_ordinates)
+	ArrayList<ArrayList<Double>> curve = new ArrayList<ArrayList<Double>>(); //created 2D arraylist
+	public Curve(ArrayList<Double> co_ordinates)//constructor which takes parameters of type Arraylist<double>
 	{	
-		int pairs = co_ordinates.size()/2;
-		int j = 0;
+		int pairs = co_ordinates.size()/2; //Calculate the number of x,y co-ordinate pairs in the the arraylist
+		int j = 0; //initialize counter to iterate through the co_ordinates ArrayList
+		
+		/*
+		*creating a 2D arraylist by storing x value and y value in each row 
+		*
+		*
+		*/
 		for (int i = 0; i < pairs; i++)
 		{
-			curve.add(new ArrayList<Double>());	
-			curve.get(i).add(0,co_ordinates.get(j));
-			curve.get(i).add(1,co_ordinates.get(j+1));
+			curve.add(new ArrayList<Double>());	//to intialze an array and add each row´s elements to this array, without this , cannot add elements to the curve arraylist !!
+			curve.get(i).add(0,co_ordinates.get(j));//x Co-ordinates
+			curve.get(i).add(1,co_ordinates.get(j+1));//y Co-ordinates
 			if (j < (co_ordinates.size() - 2))
 			{
-				curve.get(i).add(2,((co_ordinates.get(j+1) - co_ordinates.get(j+3))/(co_ordinates.get(j) - co_ordinates.get(j+2))));
+				curve.get(i).add(2,((co_ordinates.get(j+1) - co_ordinates.get(j+3))/(co_ordinates.get(j) - co_ordinates.get(j+2)))); //slope
 			}
 			
 			j = j+2;		
@@ -25,16 +30,22 @@ public class Curve {
 			
 	}
 
+
+	/**
+	 * To evaluate the value of Y at any X value
+	 * @param x
+	 * @return y
+	 */
 	public double Evaluate_Y_at_X(double x)
 	{
 		double y = 0;
 		for(int i = 0; i < curve.size()-1; i++)
 		{
-			if(x>curve.get(i).get(0) && x <= curve.get(i+1).get(0))
+			if(x>curve.get(i).get(0) && x <= curve.get(i+1).get(0))//To check the segment in which the input x value belongs to
 			{
-				double slope = curve.get(i).get(2);
-				double yIntercept = curve.get(i).get(1) - slope * curve.get(i).get(0);
-				y =  (slope * x) + yIntercept;
+				double slope = curve.get(i).get(2); // calculate slope of that segment
+				double yIntercept = curve.get(i).get(1) - slope * curve.get(i).get(0);//calculate y-intercept of that segment
+				y =  (slope * x) + yIntercept;//calculate Y value at that point x
 
 			}
 
@@ -42,6 +53,11 @@ public class Curve {
 		}
 		
 		return y;
+	}
+
+	public ArrayList<Double> PseudoInverse(){
+
+
 	}
 
 }
--- TesterCurve.java
+++ TesterCurve.java
@@ -3,23 +3,21 @@ import java.util.ArrayList;
 public class TesterCurve {
     public static void main(String[] args)
     {
-		Scanner input1 = new Scanner(System.in);
-		ArrayList<Double> co_ordinates = new ArrayList<Double>();
+		Scanner input1 = new Scanner(System.in);//input scanner
+		ArrayList<Double> co_ordinates = new ArrayList<Double>();//1D Arraylist to store input elements
 		System.out.println("Enter the co-ordinates: ");
 
-		String [] tokens = input1.nextLine().split("\\s");
+		String [] tokens = input1.nextLine().split("\\s");//store input numbers in string tokens
 
 		for (int i = 0; i < tokens.length; i++)
-		co_ordinates.add(Double.parseDouble(tokens[i]));
+		co_ordinates.add(Double.parseDouble(tokens[i])); // add input numbers to the Array
 		
-		input1.close();
+		input1.close();//close scanner object
 
-		//for(int j  = 0; j < co_ordinates.size(); j++){
 		System.out.println(co_ordinates);
-		//}
 
-		Curve curve1 = new Curve(co_ordinates);
-		double yval = curve1.Evaluate_Y_at_X(2.5);
+		Curve curve1 = new Curve(co_ordinates); // create curve object curve1
+		double yval = curve1.Evaluate_Y_at_X(2.5);//calling evaluate Y at X method, input x value is directly passed. Need to add scanner
 		System.out.println("Y value is: "+ yval);
 
     }
