670b86bad48e5d04bad8b81c44da373f32530356
Tejas <tejas.hu7@gmail.com>
2019-12-17 21:50:59 +0100

    codefrom tejas

--- TesterCurve.java
+++ TesterCurve.java
@@ -1,26 +1,24 @@
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.List;
+
 public class TesterCurve {
-    public static void main(String[] args)
-    {
-		Scanner input1 = new Scanner(System.in);
-		ArrayList<Double> co_ordinates = new ArrayList<Double>();
-		System.out.println("Enter the co-ordinates: ");
-
-		String [] tokens = input1.nextLine().split("\\s");
-
-		for (int i = 0; i < tokens.length; i++)
-		co_ordinates.add(Double.parseDouble(tokens[i]));
+	public static void main(String args[])
+	{
+		List<Double> segment_val = new ArrayList<>();
+		Curve Line = new Curve(0.0,0.0,5.0,5.0,25,0);
+		segment_val = Line.getSegmentValue(0);
+		System.out.println(segment_val);
+		double y = Line.evaluateX(2);
+		System.out.println(y);
 		
-		input1.close();
+		Line.scaleX(2);
+		//for(int i=0;i<segement_val.size();i++)
+		//System.out.println("after scaling :" + segement_val.get(i));
+	//	Line.getSegmentValue(1);
+	//	Line.getSegmentValue(2);
+	//	Line.getSegmentValue(3);
+	//	Line.getSegmentValue(4);
+	}
 
-		//for(int j  = 0; j < co_ordinates.size(); j++){
-		System.out.println(co_ordinates);
-		//}
-
-		Curve curve1 = new Curve(co_ordinates);
-		double yval = curve1.Evaluate_Y_at_X(2.5);
-		System.out.println("Y value is: "+ yval);
-
-    }
 }
+
