eecfcc67f7bf4b6d23e17e518ab7773bd86a523c
mandali-harish <51310990+mandali-harish@users.noreply.github.com>
2019-12-26 19:50:17 +0100

    Add files via upload
    Added comments to few methods
    Merged code from Tejas branch

--- Curve.java
+++ Curve.java
@@ -10,7 +10,40 @@
 		List<Segments> ds = new ArrayList<>();
 		//private double[] segment;
 		private double period = 0;
-	/*creating a curve with x-intercepts, y-intercepts and slope for different segments*/
+
+		public double getY_intercept() {
+			return y_intercept;
+		}
+
+		public void setY_intercept(double y_intercept) {
+			this.y_intercept = y_intercept;
+		}
+
+		public double getSlope() {
+			return slope;
+		}
+
+		public void setSlope(double slope) {
+			this.slope = slope;
+		}
+
+		public List<Segments> getDs() {
+			return ds;
+		}
+
+		public void setDs(List<Segments> ds) {
+			this.ds = ds;
+		}
+
+		public double getPeriod() {
+			return period;
+		}
+
+		public void setPeriod(double period) {
+			this.period = period;
+		}
+
+		/*creating a curve with x-intercepts, y-intercepts and slope for different segments*/
 		public Curve(double ...seg) 
 		{
 			//int k=1;
@@ -49,17 +82,22 @@
 			}	
 
 		}
-
-		public Curve(ArrayList<Double> curve, int flag)
+		/**
+		 * Curve Constructor which takes an input Arraylist
+		 * Heplful when creating curve objects for the Curve methods
+		 * @param curvearray Arraylist containing elements of the curve 
+		 * @param flag	flag to check the type of arraylist, flag is 1 for multiplication of 2 curves method
+		 */
+		public Curve(ArrayList<Double> curvearray, int flag)
 		{
 			if(flag==1)
 			{
 			int j=0;
-				if(curve.size() % 2 == 0)
-				{	for(int i = 1;i<=(curve.size()/2);i++)
+				if(curvearray.size() % 2 == 0)
+				{	for(int i = 1;i<=(curvearray.size()/2);i++)
 					{
 
-							Segments segment = new Segments(curve.get(j), curve.get(j+1));
+							Segments segment = new Segments(curvearray.get(j), curvearray.get(j+1));
 							ds.add(segment);		
 						j +=2;
 					}
@@ -68,11 +106,11 @@
 			else
 			{
 				int j=0;
-				if(curve.size() % 3 == 0)
-				{	for(int i = 1;i<=(curve.size()/3);i++)
+				if(curvearray.size() % 3 == 0)
+				{	for(int i = 1;i<=(curvearray.size()/3);i++)
 					{
 
-							Segments segment = new Segments(curve.get(j), curve.get(j+1),curve.get(j+2));
+							Segments segment = new Segments(curvearray.get(j), curvearray.get(j+1),curvearray.get(j+2));
 							ds.add(segment);		
 						j +=3;
 					}
@@ -82,11 +120,17 @@
 
 		}
 
-		
+		/**
+		 * evaluateYatX To Evaluate Y value at a given X co-ordinate of any curve
+		 * @param x
+		 * @return
+		 */
 		public double evaluateYatX(double x)
 		{
 			double y = 0;
-			
+			/*
+			*if the curve has no periodicity, implements below code
+			*/
 			if(period == 0)
 			{							
 				//System.out.println("number of segments: " + ds.size());
@@ -103,11 +147,14 @@
 				//System.out.println("y_intercept is : " + y_intercept);
 				//System.out.println("slope is : "+ slope);
 			}
+			/*
+			*If curve has periodicity, implements below code
+			*/
 			else
 			{
 				
 				//System.out.println("number of segments: " + (ds.size()-1));
-				if(x/period <= 1)
+				if(x/period <= 1)//Checking if period is before all the segments start
 				{
 					for(int i = 0;i<ds.size();i++)
 					{
@@ -122,7 +169,7 @@
 					//System.out.println("y_intercept is : " + y_intercept);
 					//System.out.println("slope is : "+ slope);
 				}
-				else
+				else//if the period starts after all the segments start, implements below
 				{
 					double xVal = 0;
 					//for(int i = 0;xVal>=x;i++)
@@ -194,6 +241,12 @@
 
 		}
 
+		/**
+		 * Ceil method implments the ceil of the curve for a given curve upto the given timeperiod and returns the resultant curve
+		 * @param curve1 
+		 * @param timePeriod
+		 * @return ceiledcurve
+		 */
 		public static Curve ceil(Curve curve1, int timePeriod)
 		{
 			ArrayList<Double> ceil1 = new ArrayList<>();
@@ -209,10 +262,16 @@
 					ceil1.add(slope);
 				}	
 			}
-			Curve c4 = new Curve(ceil1,2);
-			return c4;
+			Curve ceiledcurve = new Curve(ceil1,2);
+			return ceiledcurve;
 		}
 
+		/**
+		 * floor method implements the floor of the curve for a given curve upto the given timeperiod and returns the resultant curve
+		 * @param curve1
+		 * @param timePeriod
+		 * @return flooredcurve
+		 */
 		public static Curve floor(Curve curve1, int timePeriod)
 		{
 			ArrayList<Double> floor1 = new ArrayList<>();
@@ -227,13 +286,17 @@
 					floor1.add(slope);
 				}	
 			}
-			Curve c4 = new Curve(floor1,2);
-			return c4;
+			Curve flooredcurve = new Curve(floor1,2);
+			return flooredcurve;
 
 		}
 
 
-
+		/**
+		 * EvaluateXatY Method returns the value of X at a given Y of the input curve
+		 * @param y y co-ordinate 
+		 * @return x x co-ordinate
+		 */
 		public double EvaluateXatY(double y)//x = -m*y +c
 		{
 			double x = 0;
@@ -267,8 +330,9 @@
 				
 				//System.out.println(ds_X.get(i).getarrayValue(i));
 				//System.out.println(ds_X.size());
-				//System.out.println("Slope is" + slope);
+				//System.out.println("Slope is" + slope);Z
 			}
+
 			for(int i = 0; i<(ds.size() - 1);i++)
 			{
 				slope = (evaluateYatX((ds.get(i+1).getarrayValue(0)) - ds_X.get(i).getarrayValue(1))/(ds_X.get(i+1).getarrayValue(0)-ds.get(i).getarrayValue(0)));
@@ -277,25 +341,342 @@
 				
 			}
 			
-			
-			
-			
-			
-
-			
+			System.out.println(ds_X.size());
 			//return ds_X;
 		}
 
+		public List<Segments> multiplyScalar(int scalar)
+		{
+			List<Segments> ds_X = new ArrayList<>();
+			System.out.println("ds_X " + ds_X.size() + "ds_size is : " + ds.size());
+			
+			for(int i= 0 ; i < ds.size()-1;i++)
+			{
+				//ds_X.add(i, ds.get(i));
+				//System.out.println("Segment is" + ds_X.get(i).getValue());
+				ds_X.add(i,ds.get(i).multiplyScale(scalar));
+				System.out.println(ds_X.get(i).getValue());
+			}
+			//ds_X.add(ds.size(), );
+			System.out.println(ds_X.size());
+			return ds_X;
+			
+			
+		}
+
+		public static List<Segments> addCurve(Curve x,Curve y) {
+		
+			List<Segments> da = new ArrayList<>();
+			
+			int flagP =0;
+			int minSize =0;
+			double dx=0,dx1 = 0,dx2 =0;
+			double dy=0,dy1=0;
+			System.out.println("x size is "+x.size());
+			
+			if(x.size() < y.size())
+			{
+				minSize = x.size();
+			}
+			else 
+			{
+				minSize = y.size();
+			}
+			
+			System.out.println(minSize);
+			for(int i=0; i< minSize - 1;i++)
+			{
+					if(minSize==2)
+					{
+						if(y.size() == 2)
+						{
+							if(x.ds.get(i).getarrayValue(0) < y.ds.get(i).getarrayValue(0))
+							{
+								dx1 = x.ds.get(i).getarrayValue(0);
+							}
+							else 
+							{
+								dx1 = y.ds.get(i).getarrayValue(0);
+							}
+							if(x.ds.get(i).getarrayValue(1) < y.ds.get(i).getarrayValue(1))
+							{
+								dy1= x.ds.get(i).getarrayValue(1);
+							}
+							else
+							{
+								dy1 = y.ds.get(i).getarrayValue(1);
+							}
+						}
+						if(x.period > y.period)
+							dx=x.period;
+						else
+							dx=y.period;
+					}
+					else if(minSize>2)
+					{
+						if(x.period < y.period)
+						{
+							flagP = 1;
+						}
+						if(x.ds.get(i).getarrayValue(0) < y.ds.get(i).getarrayValue(0))
+						{
+							dx1 = x.ds.get(i).getarrayValue(0);
+						}
+						else 
+						{
+							dx1 = y.ds.get(i).getarrayValue(0);
+						}
+						if(x.ds.get(i).getarrayValue(1) < y.ds.get(i).getarrayValue(1))
+						{
+							dy1= x.ds.get(i).getarrayValue(1);
+						}
+						else
+						{
+							dy1 = y.ds.get(i).getarrayValue(1);
+						}
+						
+						if(flagP == 1)
+						{
+							dx= y.ds.get(i+1).getarrayValue(0);
+						}
+						else
+						{
+							dx= x.ds.get(i+1).getarrayValue(0);
+						}
+						System.out.println("dx value is :" + dx);
+					}					
+						if(i == 0)
+						{
+							double dx_first = 0;
+							if(x.size() > 2)
+							{
+								if(x.ds.get(i+1).getarrayValue(0) < y.ds.get(i+1).getarrayValue(0))
+								{
+									dx_first = x.ds.get(i+1).getarrayValue(0);
+								}
+								else
+								{
+									dx_first = y.ds.get(i+1).getarrayValue(0);
+								}
+								System.out.print("x value of first segment is" + dx);
+							}
+							else
+							{
+								if(x.period < y.period)
+									dx_first = x.period;
+								else
+									dx_first = y.period;
+							}
+							dy = x.evaluateYatX(dx_first) + y.evaluateYatX(dx_first);
+							double ds = dy/dx_first;
+							Segments firstSeg = new Segments(dx1,dy1,ds);
+							da.add(firstSeg);
+						}
+						dy = x.evaluateYatX(dx) + y.evaluateYatX(dx);
+						double dslope = dy/dx;					
+						{
+							Segments segment = new Segments(dx , dy, dslope);
+							da.add(segment);
+						}		
+						System.out.println("y value after addition is : "+ dy);
+						System.out.println("second curve x value is " + dx2);
+						System.out.println("Slope of add curve is " + dslope);									
+				}
+			
+			return da;			
+		}
+		
+		public static List<Segments> curveMax(Curve a, Curve b)
+		{
+			List<Segments> dm = new ArrayList<>();
+			int minSize =0;
+			double dx=0,dx1_a=a.ds.get(0).getarrayValue(0),dx1_b=b.ds.get(0).getarrayValue(0);
+			double dy_a=0,dy_b=0,dy1_a=a.ds.get(0).getarrayValue(0),dy1_b=b.ds.get(0).getarrayValue(0);
+			System.out.println("x size is "+a.size());
+			
+			if(a.size() < b.size())
+			{
+				minSize = a.size();
+			}
+			else
+			{
+				minSize = b.size();
+			}
+			
+			for(int i=0;i<minSize - 1;i++)
+			{
+				if(minSize == 2)
+				{					
+					if(a.period > b.period)
+					{
+						dx = b.period;
+					}
+					else
+					{
+						dx=a.period;
+					}
+					
+						dy_a = a.evaluateYatX(dx);
+						dy_b = b.evaluateYatX(dx);
+						if(dy_a>dy_b)
+						{
+							Segments seg = new Segments(dx1_a,dy1_a,a.slope);
+							dm.add(seg);
+						}
+						else
+						{
+							Segments seg = new Segments(dx1_b,dy1_b,b.slope);
+							dm.add(seg);
+						}
+				}
+				else 
+				{					
+						if(a.ds.get(i+1).getarrayValue(0) > b.ds.get(i+1).getarrayValue(0))
+						{
+							dx = b.ds.get(i+1).getarrayValue(0);
+						}
+						else
+						{
+							dx = a.ds.get(i+1).getarrayValue(0);
+						}
+						dy_a = a.evaluateYatX(dx);
+						dy_b = b.evaluateYatX(dx);
+						
+						if(dy_a > dy_b)
+						{
+							Segments seg = new Segments(a.ds.get(i).getarrayValue(0),a.ds.get(i).getarrayValue(1),a.ds.get(i).getarrayValue(2));
+							dm.add(seg);
+						}
+						else
+						{
+							Segments seg = new Segments(b.ds.get(i).getarrayValue(0),dy1_b = b.ds.get(i).getarrayValue(1),b.ds.get(i).getarrayValue(2));
+							dm.add(seg);
+						}		
+				}
+			}
+			
+			if(a.size() > minSize)
+			{
+				for (int i = minSize;i<a.size();i++)
+				{
+					dm.add(a.ds.get(i-1));
+				}
+			}
+			System.out.println("b. size is :" + b.size() + " Minsize is : "+minSize);
+			if(b.size() > minSize)
+			{
+				for (int i = minSize;i<b.size();i++)
+				{
+					dm.add(b.ds.get(i-1));
+				}
+			}
+
+			
+			return dm;			
+		}
+		
+		public static List<Segments> curveMin(Curve a, Curve b)
+		{
+			List<Segments> dmin = new ArrayList<>();
+			int minSize =0;
+			double dx=0,dx1_a=a.ds.get(0).getarrayValue(0),dx1_b=b.ds.get(0).getarrayValue(0);
+			double dy_a=0,dy_b=0,dy1_a=a.ds.get(0).getarrayValue(0),dy1_b=b.ds.get(0).getarrayValue(0);
+			System.out.println("x size is "+a.size());
+			
+			if(a.size() < b.size())
+			{
+				minSize = a.size();
+			}
+			else
+			{
+				minSize = b.size();
+			}
+			
+			for(int i=0;i<minSize - 1;i++)
+			{
+				if(minSize == 2)
+				{					
+					if(a.period > b.period)
+					{
+						dx = b.period;
+					}
+					else
+					{
+						dx=a.period;
+					}
+					
+						dy_a = a.evaluateYatX(dx);
+						dy_b = b.evaluateYatX(dx);
+						if(dy_a<dy_b)
+						{
+							Segments seg = new Segments(dx1_a,dy1_a,a.slope);
+							dmin.add(seg);
+						}
+						else
+						{
+							Segments seg = new Segments(dx1_b,dy1_b,b.slope);
+							dmin.add(seg);
+						}
+				}
+				else 
+				{					
+						if(a.ds.get(i+1).getarrayValue(0) > b.ds.get(i+1).getarrayValue(0))
+						{
+							dx = b.ds.get(i+1).getarrayValue(0);
+						}
+						else
+						{
+							dx = a.ds.get(i+1).getarrayValue(0);
+						}
+						dy_a = a.evaluateYatX(dx);
+						dy_b = b.evaluateYatX(dx);
+						
+						if(dy_a < dy_b)
+						{
+							Segments seg = new Segments(a.ds.get(i).getarrayValue(0),a.ds.get(i).getarrayValue(1),a.ds.get(i).getarrayValue(2));
+							dmin.add(seg);
+						}
+						else
+						{
+							Segments seg = new Segments(b.ds.get(i).getarrayValue(0),dy1_b = b.ds.get(i).getarrayValue(1),b.ds.get(i).getarrayValue(2));
+							dmin.add(seg);
+						}		
+				}
+			}
+			if(a.size() > minSize)
+			{
+				for (int i = minSize;i<a.size();i++)
+				{
+					dmin.add(a.ds.get(i-1));
+				}
+			}
+			System.out.println("b. size is :" + b.size() + " Minsize is : "+minSize);
+			if(b.size() > minSize)
+			{
+				for (int i = minSize;i<b.size();i++)
+				{
+					dmin.add(b.ds.get(i-1));
+				}
+			}
+			return dmin;			
+		}
+
+		/*gets the value of an element from the curve arraylist*/
 		public double getCellValue(int Cval, int Cval1){
 
 			return  ds.get(Cval).getarrayValue(Cval1);
 		}
+		
+		/**
+		 * size returns the size of the curve
+		 * @return
+		 */
 		public int size()
 		{
 			return ds.size();
 
 		}
-/*getting the value of each segment*/
+		/*getting the value of each segment*/
 		public List getSegmentValue(int get) {
 			
 			return  ds.get(get).getValue();
@@ -311,6 +692,15 @@
 					System.out.print(" " + ds.get(i).getarrayValue(j));
 				}
 			}
+			System.out.println();
+		}
+
+		public double getX_intercept() {
+			return x_intercept;
+		}
+
+		public void setX_intercept(double x_intercept) {
+			this.x_intercept = x_intercept;
 		}
 	}
 
--- TesterCurve.java
+++ TesterCurve.java
@@ -2,44 +2,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TesterCurve {
+	@SuppressWarnings("unchecked")
 	public static void main(String args[])
 	{
-		//List<Double> segment_val = new ArrayList<>();
-		Curve Line = new Curve(0,0,1,2,2,0.25,5);
+		List<Segments> segment_val = new ArrayList<>();
+		Curve Line1 = new Curve(0,0,1,2,2,0.25,5);
 		//System.out.println(segment_val);
 		//double y = Line.evaluateYatX(38);
 		//System.out.println("Y evaluated at x is "+y);
-		for(int i =0; i<Line.size();i++){
-			System.out.println(Line.getSegmentValue(i));
-		}
-		Curve Line1 = new Curve(0,5,1,10,15,2,15);
-		for(int i =0; i<Line1.size();i++){
-			System.out.println(Line1.getSegmentValue(i));
-		}
+		//for(int i =0; i<Line1.size();i++){
+		//	System.out.println(Line1.getSegmentValue(i));
+		//}
+		Curve Line2 = new Curve(0,5,1,10,15,2,15);
+		
+		//for(int i =0; i<Line2.size();i++){
+		//	System.out.println(Line2.getSegmentValue(i));
+		//}
+
+
+		segment_val = Curve.addCurve(Line1, Line2);
+		//segment_val = Curve.curveMin(Line1, Line2);
+		Curve.getCurve(segment_val);
 		
 		Curve test;
-		test = Curve.floor(Line,20);
+		test = Curve.floor(Line1,20);
 		for(int i =0; i<test.size();i++){
 			System.out.println(test.getSegmentValue(i));
 		}
+		
+		Curve test1;
+		test1 = Curve.ceil(Line1,20);
+		for(int i =0; i<test1.size();i++){
+			System.out.println(test1.getSegmentValue(i));
+		}
+
+
+
+
 		//Curve mul1;
 		//mul1 = Curve.multiplicationOfTwoCurves(Line,Line1,20);
 		//for(int i =0; i<mul1.size();i++){
 		//	System.out.println(mul1.getSegmentValue(i));
 		//}
+
 		//double y1 = Line.evaluateYatX(15);
 		//System.out.println("Y1 evaluated at x is "+y1);
+
 		//double y = Line1.evaluateYatX(12);
 		//System.out.println("Y evaluated at x is "+y);
+
 		//for(int i =0; i<Line1.size();i++){
 		//	System.out.println(Line1.getSegmentValue(i));
 		//}
 		//for(int i =0; i<Line.size();i++){
 		//	System.out.println(Line.getSegmentValue(i));
 		//}
+
 		//Line.scaleX(2);
 		//for(int i=0;i<segement_val.size();i++);
 		//System.out.println("after scaling :" + segement_val.get(i));
+
+
 	//	Line.getSegmentValue(1);
 	//	Line.getSegmentValue(2);
 	//	Line.getSegmentValue(3);
